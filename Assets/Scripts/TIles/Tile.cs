87c206913880b879bd371732a3d0e2199d2f1fc0
Maider Sanchez <maidersanchezcasas@gmail.com>
2024-02-20 12:52:12 +0100

    wip

--- Assets/Scripts/TIles/Tile.cs
+++ Assets/Scripts/TIles/Tile.cs
@@ -1,34 +1,50 @@
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+namespace CrazyRubberProject
 {
-    [SerializeField] private GameObject[] decorationAreas;
-    [SerializeField] private GameObject[] decorationAssets;
-    [SerializeField] private int numberOfProps;
-
-    private void OnEnable()
+    public class Tile : MonoBehaviour
     {
-        DecorateTile();
-    }
+        [SerializeField] private GameObject[] decorationAreas;
+        [SerializeField] private GameObject[] decorationAssets;
+        [SerializeField] public GameObject anchorPoint;
+        [SerializeField] private GameObject decoContainer;
+        [SerializeField] private int numberOfProps;
 
-    private void DecorateTile()
-    {
-        for (int i = 0; i < numberOfProps; i++)
+        public delegate void PlayerEntered(Tile myTile);
+        public static event PlayerEntered onPlayerEntered;
+
+        private void OnEnable()
         {
-            // Elegir aleatoriamente un área de decoración y un activo de decoración
-            GameObject selectedArea = decorationAreas[Random.Range(0, decorationAreas.Length)];
-            GameObject selectedAsset = decorationAssets[Random.Range(0, decorationAssets.Length)];
-
-            // Obtener una posición aleatoria dentro del área de decoración
-            Vector3 randomPosition = selectedArea.transform.position + new Vector3(Random.Range(-selectedArea.transform.localScale.x / 2f, selectedArea.transform.localScale.x / 2f),
-                                                                                     0f,
-                                                                                     Random.Range(-selectedArea.transform.localScale.z / 2f, selectedArea.transform.localScale.z / 2f));
-
-            // Crear una instancia del activo de decoración en la posición aleatoria
-            GameObject newDecoration = Instantiate(selectedAsset, randomPosition, Quaternion.identity);
-            // Asegurarse de que el activo de decoración esté dentro del área de decoración
-            newDecoration.transform.parent = selectedArea.transform;
+            DecorateTile();
         }
-    }
 
+        private void DecorateTile()
+        {
+            for (int i = 0; i < numberOfProps; i++)
+            {
+                // Elegir aleatoriamente un área de decoración y un activo de decoración
+                GameObject selectedArea = decorationAreas[Random.Range(0, decorationAreas.Length)];
+                GameObject selectedAsset = decorationAssets[Random.Range(0, decorationAssets.Length)];
+
+                // Obtener una posición aleatoria dentro del área de decoración
+                Vector3 randomPosition = selectedArea.transform.position + new Vector3(Random.Range(-selectedArea.transform.localScale.x / 2f, selectedArea.transform.localScale.x / 2f),
+                                                                                         0f,
+                                                                                         Random.Range(-selectedArea.transform.localScale.z / 2f, selectedArea.transform.localScale.z / 2f));
+
+                // Crear una instancia del activo de decoración en la posición aleatoria
+                GameObject newDecoration = Instantiate(selectedAsset, randomPosition, Quaternion.identity);
+                // Asegurarse de que el activo de decoración esté dentro del área de decoración
+                newDecoration.transform.parent = decoContainer.transform;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                onPlayerEntered?.Invoke(this);
+            }
+        }
+
+    }
 }
--- Assets/Scripts/TIles/TileManager.cs
+++ Assets/Scripts/TIles/TileManager.cs
@@ -1,39 +1,105 @@
 using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class TileManager : MonoBehaviour
+namespace CrazyRubberProject
 {
-    [SerializeField] private Tile[] tiles;
-    [SerializeField] private int numberOfTiles;
-    [SerializeField] private GameObject tileCointainer;
-
-    private int tileLength = 10;
-    private int tileIndex = 0;
-    private Transform rubberPos;
-
-    private Tile currentTile;
-    void Start()
+    public class TileManager : MonoBehaviour
     {
-        for (int i = 0; i< numberOfTiles; i++)
+        [SerializeField] private Tile[] tiles;
+        [SerializeField] private int numberOfTiles;
+        [SerializeField] private GameObject tileContainer;
+
+        private int tileLength = 10;
+        private int tileIndex = 0;
+        private int speed = 3;
+        private bool hasSpawn = false;
+
+        private Tile currentTile;
+
+        private void Awake()
         {
-            SpawnTile();
-            tileIndex++;
+            Tile.onPlayerEntered += SelectCurrentTile;
+        }
+        void Start()
+        {
+            for (int i = 0; i < numberOfTiles; i++)
+            {
+                SpawnTile(tileIndex);
+                tileIndex++;
+            }
+
+            StartCoroutine(TilesMovement());
         }
 
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            if (CheckPlayerProximity() && !hasSpawn)
+            {
+                SpawnTile(tileIndex);
+                tileIndex++;
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private void SpawnTile(int newTileIndex)
+        {
+            Tile newTile = Instantiate(tiles[Random.Range(0, tiles.Length)]);
+            newTile.transform.position = new Vector3(0, 0, -(newTileIndex * tileLength));
+            newTile.transform.SetParent(tileContainer.transform);
 
-    private void SpawnTile()
-    {
-        Tile newTile = Instantiate(tiles[Random.Range(0, tiles.Length)]);
+        }
 
-        newTile.transform.position = new Vector3(0, 0, tileIndex * tileLength);
+        private void SelectCurrentTile(Tile myTile)
+        {
+            currentTile = myTile;
+            hasSpawn = false;
+        }
 
+        private bool CheckPlayerProximity()
+        {        
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            if (currentTile != null && player != null)
+            {
+                float playerPos = player.transform.position.z;
+                float tileEndPoint = currentTile.anchorPoint.transform.position.z;
+
+                if (tileEndPoint - playerPos < 1)
+                {
+                    hasSpawn = true;    
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        IEnumerator TilesMovement()
+        {
+            while (true)
+            {
+                MoveTiles();
+                yield return null;
+            }
+        }
+
+        void MoveTiles()
+        {
+            Vector3 newPosition = tileContainer.transform.position;
+            newPosition.z += speed * Time.deltaTime;
+            tileContainer.transform.position = newPosition;
+        }
+
+        private void OnDestroy()
+        {
+            Tile.onPlayerEntered -= SelectCurrentTile;
+        }
     }
 }
