72d75c062e54c54f9ed64d6cae41fb6b44a4a6ec
ArkaitzL <127892239+ArkaitzL@users.noreply.github.com>
2024-02-21 08:51:58 +0100

    Puntuacion

---
+++ Assets/Scripts/Controlador.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Controlador : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI puntuacionTxt, dineroTxt;
+
+    private float puntuacion = 0f;
+    public static Controlador ins;
+
+    private void Awake()
+    {
+        ins = this;
+    }
+
+    private void Start()
+    {
+        Dinero();
+    }
+
+    private void Update()
+    {
+        puntuacion += Time.deltaTime * 10f;
+        puntuacionTxt.text = puntuacion.ToString("F0");
+    }
+
+    public void Dinero() 
+    {
+        dineroTxt.text = Save.Data.monedas.ToString();
+    }
+
+    public void Muerto() 
+    {
+        if (Save.Data.mejor_puntuacion < puntuacion) Save.Data.mejor_puntuacion = puntuacion;
+        
+        //*** MenuMuerte/Cambio a menu principal ***
+    }
+}
